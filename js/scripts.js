6477f6c1200a3f73f6eb5d134396fc2467ad291a
moliver05 <mo.mangahas@yahoo.com>
2018-08-17 12:46:53 -0700

    added css and fixed javascript

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// business logic
+var converter = function(num){
+  var result = [];
+
+
+  if(num < 0){
+    return "I suggest a better number."
+    }else if(num > 0 && num%3 === 0){
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if(num.toString().indexOf('1') > -1){
+    return "Boop!";
+    } else if(num%10 === 0){
+    return "Beep!";
+    } else {
+    for(var i = 0; i <= num; i += 1){
+      result.push(i);
+      };
+    }
+    return result.toString();
+  }
+
+// user interface
+$(document).ready(function(){
+  $("#magicform").submit(function(event){
+    event.preventDefault();
+    var userInput = parseInt($('input#inputOne').val());
+    var userResult = converter(userInput);
+    $("#outputR").text(userResult);
+    $('#magicform').trigger('reset');
+  });
+});
