8cd341400d4d7fb110ac93f4abf16c3846c0975f
moliver05 <mo.mangahas@yahoo.com>
2018-08-17 10:45:52 -0700

    2nd commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,31 @@
-// Back End
-var convert = function(num) {
+// search for number
+function hasNumber(string) {
+  var regex = /[0-999]/;
+  return regex.test(string);
+}
 
-  // Arrays with their Matching Values
-  var numberValue = [ '1000', '900', '500', '400', '100', '99', '90', '50', '40', '10', '9', '5', '4', '1' ];
-  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XCIX', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
-  var romanChanger = '';
+//
+var translate = function(num) {
+  var stringZero = [ '0' ];
+  var stringOne = [ '1' ];
+  var numberThree = [ '3' ]
+  // var regexp =  new RegExp("\\d+", "g");
+}
 
-    // Loop through the indices of the number Value array.
-    for (var index = 0; index < numberValue.length; index++) {
-      while (+numberValue[index] <= num) {
-        romanChanger += romanNumeral[index];
-        num -= numberValue[index];
-    }
-  }
-  return romanChanger;
+  return numChanger;
 };
+
+
+
+
 
 //Front End
 $(document).ready(function() {
-  $("form#romanized").submit(function(event) {
+  $("form#changer").submit(function(event) {
     event.preventDefault();
     // var numeral = parseInt($("input#numeral").val());
     var result = $("#enternum").val();
-       $("#result").text(convert(result));
+       $("#result").text(translate(result));
        $("#result").show();
      });
    });
