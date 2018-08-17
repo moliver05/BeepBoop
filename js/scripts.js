9f43190c339bec584b600c96e1646f5b41af71b2
moliver05 <mo.mangahas@yahoo.com>
2018-08-17 13:03:16 -0700

    fixed bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // business logic
 var converter = function(num){
   var result = [];
@@ -30,36 +29,3 @@ $(document).ready(function(){
     $('#magicform').trigger('reset');
   });
 });
-=======
-// search for number
-function hasNumber(string) {
-  var regex = /[0-999]/;
-  return regex.test(string);
-}
-
-//
-var translate = function(num) {
-  var stringZero = [ '0' ];
-  var stringOne = [ '1' ];
-  var numberThree = [ '3' ]
-  // var regexp =  new RegExp("\\d+", "g");
-}
-
-  return numChanger;
-};
-
-
-
-
-
-//Front End
-$(document).ready(function() {
-  $("form#changer").submit(function(event) {
-    event.preventDefault();
-    // var numeral = parseInt($("input#numeral").val());
-    var result = $("#enternum").val();
-       $("#result").text(translate(result));
-       $("#result").show();
-     });
-   });
->>>>>>> 8cd341400d4d7fb110ac93f4abf16c3846c0975f
